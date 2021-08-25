c97587754562dd2dd3c12781b45ca64189c30989
Sergio Piña <alonzopina7@gmail.com>
2021-08-25 00:12:51 -0500

    calculadora v1

--- Calculadora.js
+++ Calculadora.js
@@ -4,37 +4,114 @@ import {View, Text, StyleSheet, Button} from 'react-native';
 export default class Calculadora extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      resultado: '',
+      temp: '',
+      operacion: '',
+    };
   }
 
   render() {
+    const cero = () => {
+      this.setState({resultado: this.state.resultado + 0});
+    };
+    const uno = () => {
+      this.setState({resultado: this.state.resultado + 1});
+    };
+    const dos = () => {
+      this.setState({resultado: this.state.resultado + 2});
+    };
+    const tres = () => {
+      this.setState({resultado: this.state.resultado + 3});
+    };
+    const cuatro = () => {
+      this.setState({resultado: this.state.resultado + 4});
+    };
+    const cinco = () => {
+      this.setState({resultado: this.state.resultado + 5});
+    };
+    const seis = () => {
+      this.setState({resultado: this.state.resultado + 6});
+    };
+    const siete = () => {
+      this.setState({resultado: this.state.resultado + 7});
+    };
+    const ocho = () => {
+      this.setState({resultado: this.state.resultado + 8});
+    };
+    const nueve = () => {
+      this.setState({resultado: this.state.resultado + 9});
+    };
+    const suma = () => {
+      this.setState({temp: this.state.resultado});
+      this.setState({resultado: ''});
+      this.setState({operacion: 'suma'});
+    };
+    const resta = () => {
+      this.setState({temp: this.state.resultado});
+      this.setState({resultado: ''});
+      this.setState({operacion: 'resta'});
+    };
+    const multiplicacion = () => {
+      this.setState({temp: this.state.resultado});
+      this.setState({resultado: ''});
+      this.setState({operacion: 'multiplicacion'});
+    };
+    const division = () => {
+      this.setState({temp: this.state.resultado});
+      this.setState({resultado: ''});
+      this.setState({operacion: 'division'});
+    };
+    const limpiar = () => {
+      this.setState({temp: ''});
+      this.setState({resultado: ''});
+      this.setState({operacion: ''});
+    };
+    const btnResultado = () => {
+      if (this.state.operacion === 'suma') {
+        var res = parseInt(this.state.temp) + parseInt(this.state.resultado);
+        this.setState({resultado: res});
+      } else if (this.state.operacion === 'resta') {
+        var res = parseInt(this.state.temp) - parseInt(this.state.resultado);
+        this.setState({resultado: res});
+      } else if (this.state.operacion === 'multiplicacion') {
+        var res = parseInt(this.state.temp) * parseInt(this.state.resultado);
+        this.setState({resultado: res});
+      } else if (this.state.operacion === 'division') {
+        var res = parseInt(this.state.temp) / parseInt(this.state.resultado);
+        this.setState({resultado: res});
+      }
+    };
     return (
       <View>
         <Text style={styles.titulo}> Calculadora </Text>
+        <View style={styles.textBox}>
+          <Text style={styles.resultado}>{this.state.resultado}</Text>
+        </View>
         <View style={styles.box}>
           <View style={styles.columna1}>
             <View style={styles.btn1}>
-              <Button title="7"></Button>
+              <Button title="7" onPress={siete}></Button>
             </View>
             <View style={styles.btn1}>
-              <Button title="4"></Button>
+              <Button title="4" onPress={cuatro}></Button>
             </View>
             <View style={styles.btn1}>
-              <Button title="1"></Button>
+              <Button title="1" onPress={uno}></Button>
             </View>
             <View style={styles.btn1}>
-              <Button title="0"></Button>
+              <Button title="0" onPress={cero}></Button>
             </View>
           </View>
           <View style={styles.columna1}>
             <View style={styles.btn1}>
-              <Button title="8"></Button>
+              <Button title="8" onPress={ocho}></Button>
             </View>
             <View style={styles.btn1}>
-              <Button title="5"></Button>
+              <Button title="5" onPress={cinco}></Button>
             </View>
             <View style={styles.btn1}>
-              <Button title="2"></Button>
+              <Button title="2" onPress={dos}></Button>
             </View>
             <View style={styles.btn1}>
               <Button title="."></Button>
@@ -42,38 +119,35 @@ export default class Calculadora extends Component {
           </View>
           <View style={styles.columna1}>
             <View style={styles.btn1}>
-              <Button title="9"></Button>
+              <Button title="9" onPress={nueve}></Button>
             </View>
             <View style={styles.btn1}>
-              <Button title="6"></Button>
+              <Button title="6" onPress={seis}></Button>
             </View>
             <View style={styles.btn1}>
-              <Button title="3"></Button>
+              <Button title="3" onPress={tres}></Button>
             </View>
             <View style={styles.btn1}>
-              <Button title="="></Button>
+              <Button title="=" onPress={btnResultado}></Button>
             </View>
           </View>
           <View style={styles.columna2}>
             <View style={styles.btn1}>
-              <Button title="CE"></Button>
+              <Button title="CE" onPress={limpiar}></Button>
             </View>
             <View style={styles.btn2}>
-              <Button title="/"></Button>
+              <Button title="/" onPress={division}></Button>
             </View>
             <View style={styles.btn2}>
-              <Button title="x"></Button>
+              <Button title="x" onPress={multiplicacion}></Button>
             </View>
             <View style={styles.btn2}>
-              <Button title="-"></Button>
+              <Button title="-" onPress={resta}></Button>
             </View>
             <View style={styles.btn2}>
-              <Button title="+"></Button>
+              <Button title="+" onPress={suma}></Button>
             </View>
           </View>
-        </View>
-        <View style={styles.textBox}>
-          <Text style={styles.resultado}>resultado</Text>
         </View>
       </View>
     );
@@ -99,13 +173,16 @@ const styles = StyleSheet.create({
   },
   columna1: {
     padding: 10,
+    paddingTop: 0,
   },
   columna2: {
     padding: 10,
+    paddingTop: 0,
     paddingLeft: 50,
   },
   textBox: {
     padding: 24,
+    paddingBottom: 0,
   },
   resultado: {
     marginTop: 16,
@@ -121,6 +198,7 @@ const styles = StyleSheet.create({
   },
   box: {
     padding: 20,
+    paddingTop: 0,
     alignItems: 'flex-start',
     flexDirection: 'row',
     flexWrap: 'wrap',
